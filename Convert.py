81d29bad3a2a319c530ae4f6ebf4cdc8e7bb03d5
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-11-02 11:59:58 -0400

    work

--- Convert.py
+++ Convert.py
@@ -52,8 +52,11 @@ if __name__ == "__main__":
     freeze_support()
 
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
-
+    logging.basicConfig(
+        format="%(asctime)s: %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
     parser = argparse.ArgumentParser(description="Add background subtraction to videos")
     parser.add_argument(
         "--path", help="the path to the video", required=False, type=str, default="."
