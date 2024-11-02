1cace6b2df6d31756edd29d00ac963a6ef8f8003
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-11-02 12:29:16 -0400

    update converter

--- Convert.py
+++ Convert.py
@@ -34,7 +34,7 @@ def convert_video(subtract_type, file, old_video_repository):
             ret, frame = cap.read()
             if not ret:
                 break
-            if count % 10000 == 0:
+            if count % 10000 == 0 and count != 0:
                 logging.info(f"Processing frame {count} of {file}")
             fgMask = subtractor.apply(frame)
             masked = cv2.bitwise_and(frame, frame, mask=fgMask)
@@ -50,8 +50,6 @@ def convert_video(subtract_type, file, old_video_repository):
 
 if __name__ == "__main__":
     freeze_support()
-
-    format = "%(asctime)s: %(message)s"
     logging.basicConfig(
         format="%(asctime)s: %(message)s",
         level=logging.INFO,
